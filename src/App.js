f498d20e9298f5d1407921718f794b8ed556d225
yuanyu90221 <yuanyu90221@yuanyu9221deMBP.mshome.net>
2018-04-06 01:46:06 +0800

    add navigation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ReduxThunk from 'redux-thunk';
 import firebase from 'firebase';
 import reducers from './reducers';
 import LoginForm from './components/LoginForm';
+import Router from './Router';
 class App extends Component {
   componentWillMount() {
     const config = {
@@ -22,7 +23,7 @@ class App extends Component {
     const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
     return (
       <Provider store={store}>
-        <LoginForm/>
+        <Router/>
       </Provider>
     )
   }
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase';
+import {Actions} from 'react-native-router-flux';
 import { 
   EMAIL_CHANGED, 
   PASSWD_CHANGED,
@@ -43,4 +44,6 @@ const loginUserSuccess = (dispatch, user) => {
     type: LOGIN_USER_SUCCESS,
     payload: user
   });
+
+  Actions.employeeList();
 };
